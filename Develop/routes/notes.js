2778360f1f0a844c57bbdc8d1c5aae650ac6293b
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-02-24 22:21:43 -0600

    commit

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,6 +1,6 @@
 const fb = require('express').Router();
-//const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
-//const uuid = require('../helpers/uuid');
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('../helpers/uuid');
 
 
 
@@ -11,3 +11,25 @@ fb.get('/', (req, res) => {
   
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
   });
+
+  // POST Route for a new UX/UI tip
+fb.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Tip added successfully 🚀`);
+  } else {
+    res.error('Error in adding tip');
+  }
+});
+  module.exports = fb;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-//const api = require('./routes/index.js');
+const api = require('./routes/index.js');
 
 const PORT = 3001;
 
@@ -9,7 +9,7 @@ const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//app.use('/api', api);
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -22,6 +22,10 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
+
+app.get('/*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+); 
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
