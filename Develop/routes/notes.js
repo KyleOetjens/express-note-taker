11018e0b76a47fde6cbe2a2fad07b04a15d166e0
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-02-25 12:38:49 -0600

    commit

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -32,4 +32,26 @@ fb.post('/', (req, res) => {
     res.error('Error in adding tip');
   }
 });
+
+fb.put('/:note_id', (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
+  console.log(req.body);
+
+  const { note_id } = req.body;
+
+  if (req.body) {
+    const editNote = {
+      note_id
+    };
+
+    readFromFile(editNote, './db/db.json');
+    res.json(`Tip obtained successfully 🚀`);
+  } else {
+    res.error('Error in getting tip');
+  }
+});
+/*fb.put('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  res.send('Got a PUT request at /user')
+})*/
   module.exports = fb;
