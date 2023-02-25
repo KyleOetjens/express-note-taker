e34d20b68fa24d8e43cb7fc31a3e84376d48c711
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-02-24 20:56:52 -0600

    commit

---
+++ Develop/routes/notes.js
@@ -0,0 +1,13 @@
+const fb = require('express').Router();
+//const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+//const uuid = require('../helpers/uuid');
+
+
+
+
+// GET Route for retrieving all the feedback
+fb.get('/', (req, res) => {
+    console.info(`${req.method} request received for feedback`);
+  
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
