d2b692a8e3962138cb684e8991cbf8a6f2dabcb7
KyleOetjens <118133483+KyleOetjens@users.noreply.github.com>
2023-03-02 17:51:06 -0600

    updatedlogs

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -7,7 +7,6 @@ const { json } = require('express');
 
 // GET Route for retrieving all the feedback
 router.get('/', (req, res) => {
-  console.log(`in get request`);
     console.info(`${req.method} request received for feedback`);
   
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
@@ -16,8 +15,6 @@ router.get('/', (req, res) => {
   // POST Route for a new UX/UI tip
 router.post('/', (req, res) => {
   console.info(`${req.method} request received to add a tip`);
-  console.log(req.body);
-
   const { title, text } = req.body;
 
   if (req.body) {
@@ -33,73 +30,26 @@ router.post('/', (req, res) => {
     res.error('Error in adding tip');
   }
 });
-
-
 // Delete a note
 router.delete(`/:id`, (req, res) => {
   console.info(`${req.method} request received to ${req.method} a note`);
-  console.log(termData);
   const noteId = req.params.id
-  console.log(noteId);
-  const newTerms = readFromFile('./db/db.json').then((data) => {
-   // parsedItem = [].concat(JSON.parse(data));
-   // return newTerms;
+  const newTerms = readFromFile('./db/db.json',`utf-8`).then((data) => {
     try {
       parsedItem = [].concat(JSON.parse(data));
     } catch (err) {
       console.log(err);
       parsedItem = [];
     }
+    return parsedItem
   })
   .then((data)=>{
-    data.filter(note => note.id !== noteId)})
-    .then((data)=>{
-      console.log(data);
+    data = data.filter(note => note.id !== noteId)
       writeToFile('./db/db.json',data);
-    })
+    
     return res.json('deleted');
-  });
+  })
+});
   
   module.exports = router;
 
-//CODE I AM JUST PLAYING WITH
-/*
-  fb.delete('/', (req, res) => {
-    console.info(`${req.method} request received to add a tip`);
-    console.log(req.body);
-  
-    const { id } = req.body;
-  
-    if (req.body) {
-      const delNote = {
-        title,
-        text,
-        id,
-      };
-  
-      writeToFile(delNote, './db/db.json');
-      res.json(`Tip deleted successfully 🚀`);
-    } else {
-      res.error('Error in deleting tip');
-    }
-  });
-
-  fb.delete(`/api/notes/:id`, (req, res) => {
-    console.info(`${req.method} request received to add a tip`);
-    console.log(req.body);
-  
-    const noteId = req.params.id
-    // Iterate through the terms name to check if it matches `req.params.term`
-    for (let i = 0; i < termData.length; i++) {
-      if (noteId === termData[i].id) {
-        writeToFile(noteId, './db/db.json');
-      }
-    }
-  
-    // Return a message if the term doesn't exist in our DB
-    return res.json('No match found');
-  });
-
-  writeToFile(delNote, './db/db.json');
-
-*/
